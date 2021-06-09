f45d164876e3ccd35528bab31beb24420dbea1c6
RoyHarrow <52362250+RoyHarrow@users.noreply.github.com>
2021-06-09 20:46:41 +0100

    Create Classify-IP01.py
    Initial version

---
+++ Classify-IP01.py
@@ -0,0 +1,23 @@
+# Python Program to classify an IP address
+#
+# Potential subroutines to use
+#
+# 1.  ip2geotools
+#
+#     https://pypi.org/project/ip2geotools/
+#
+#     $ pip install ip2geotools
+#
+from ip2geotools.databases.noncommercial import DbIpCity
+response = DbIpCity.get('147.229.2.90', api_key='free')
+print ("response.ip_address:", response.ip_address)
+print ("response.city:",       response.city )
+print("response.region:",      response.region )
+print("response.country:",     response.country )
+print("response.latitude:",    response.latitude )
+print("response.longitude:",   response.longitude )
+print("response.to_json():,    response.to_json() )
+print("response.to_xml():",    response.to_xml() )
+#
+print("End")
+#
